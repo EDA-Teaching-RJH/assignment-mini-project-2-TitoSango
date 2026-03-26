c3423d3a00445aeb4e52a003d7cd53c5372ff404
TitoSango <os335@kent.ac.uk>
2026-03-26 14:33:10 +0000

    added person and student classes added inheritance

---
+++ assignmewnt 2/student.py
@@ -0,0 +1,23 @@
+# Parent class representing a person
+class Person:
+    def __init__(self, name, email):
+        self.name = name
+        self.email = email
+
+
+# Student class inherits from Person
+class Student(Person):
+    def __init__(self, name, email, mark):
+        super().__init__(name, email)
+        self.mark = mark
+
+    # Calculate grade based on mark
+    def get_grade(self):
+        if self.mark >= 70:
+            return "A"
+        elif self.mark >= 60:
+            return "B"
+        elif self.mark >= 50:
+            return "C"
+        else:
+            return "F"
